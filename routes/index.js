268c3872857aee03c726eba4e0f1506e4b2b09bc
binitgrx <binitgurung10@gmail.com>
2025-01-02 16:41:58 +0545

    bookmark feature added

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const userRouter = require("../modules/users/user.route")
 const blogRouter = require ("../modules//blogs/blog.route")
+const bookmarkRouter =require("../modules/bookmarks/bookmark.route");
 router.get("/", (res, req, next) => {
   try {
     res.json({data:null,msg:"api is working"})
@@ -9,6 +10,7 @@ router.get("/", (res, req, next) => {
   }
 });
 
+router.use("/bookmarks",bookmarkRouter);
 router.use("/users",userRouter)
 router.use("/blogs",blogRouter)
 
